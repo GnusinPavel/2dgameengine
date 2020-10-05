4b6c10f8ec706193d7a9fc2861af0ad7c1dbf509
Gnusin Pavel <pavel.sin.gnusin@gmail.com>
2020-10-05 10:20:30 +0300

    add components into entity

--- src/Entity.h
+++ src/Entity.h
@@ -28,6 +28,15 @@ public:
     void Render();
     void Destroy();
     bool IsActive() const;
+
+    template<typename T, typename ...TArgs>
+    T& AddComponent(TArgs&&... args) {
+        T* newComponent(new T(std::forward<TArgs>(args)...));
+        newComponent->owner = this;
+        components.emplace_back(newComponent);
+        newComponent->Initialize();
+        return *newComponent;
+    }
 };
 
 #endif //ENTITY_H
